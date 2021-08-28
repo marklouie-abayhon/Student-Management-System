30c052224737b22b97b232f8b1a21d3958e7f115
Zed-55 <87111821+Zed-55@users.noreply.github.com>
2021-08-28 08:16:43 +0800

    updated by Zed-55

---
+++ database/migrations/2021_08_26_021914_create_subject_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubjectTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subject', function (Blueprint $table) {
+            $table->id();
+            $table->string('subjects');
+            $table->integer('units');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subject');
+    }
+}
