e0f23534c2c6b4d384732f539680de15c30f63e9
marklouie-abayhon <61726331+marklouie-abayhon@users.noreply.github.com>
2021-08-28 08:14:13 +0800

    Add files via upload

---
+++ database/migrations/2021_01_28_194100_create_student_models_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_models', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            
+
+ $table->string('name');
+ $table->string('img');
+ $table->string('phone');
+ $table->unsignedInteger('course_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_models');
+    }
+}
